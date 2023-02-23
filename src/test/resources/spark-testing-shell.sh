aca18aaa779531dafd9258ddc58eef99ed9ee949
Dustin Smith <Dustin.William.Smith@gmail.com>
2023-02-23 13:29:53 +0100

    update run script to use configs not on path for testing

--- src/test/resources/spark-testing-shell.sh
+++ src/test/resources/spark-testing-shell.sh
@@ -1,5 +1,6 @@
-export JOPTS=" -Dconfig.file=${HOME}/Documents/code/data-pipeline/src/test/resources/pipeline.conf -Dapplication.file=${HOME}/Documents/code/data-pipeline/src/test/resources/application.conf"
+export JOPTS=" -Dconfig.file=${HOME}/Desktop/pipeline-alt.conf -Dapplication.file=${HOME}/Desktop/application-alt.conf"
 $SPARK_HOME/bin/spark-shell \
---packages "com.typesafe:config:1.4.2,com.github.pureconfig:pureconfig_2.12:0.17.2" \
+--packages "com.typesafe:config:1.4.2,com.github.pureconfig:pureconfig_2.12:0.17.2,com.growingintech:spark-input-sources_2.12:1.0.1" \
 --driver-java-options="${JOPTS}" \
---conf "spark.executor.extraJavaOptions=${JOPTS}"
+--conf "spark.executor.extraJavaOptions=${JOPTS}" \
+--conf "spark.jars.ivy=${HOME}/dustin/.ivy2"
